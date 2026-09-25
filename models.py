1fc5603821c19247e25cddc36e51b08221775e55
rovizi <145874081+rovizi@users.noreply.github.com>
2026-09-25 12:37:23 -0300

    Update models.py

--- models.py
+++ models.py
@@ -3,13 +3,13 @@ from sqlalchemy import Column, Integer, String
 
 
 class PartidaModel(Base):
-  __tablename__ = "partidas_selecao"
+  __tablename__ = "partidas"
 
   id = Column(Integer, primary_key=True, index=True)
   selecao = Column(String, index=True)
   adversario = Column(String, index=True)
-  data = Column(String)  # Formato YYYY-MM-DD
-  horario = Column(String)  # Formato HH:MM
+  data = Column(String)
+  horario = Column(String)
   placar = Column(String)
-  status = Column(String)  # 'Finalizado', 'Ao Vivo', 'Agendados'
+  status = Column(String)
   campeonato = Column(String)
